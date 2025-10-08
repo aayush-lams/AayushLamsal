cde0c5d2903ec6412d955a6ca0da8471df0ae434
Asur-zx1 <lamsal.aayush9115@gmail.com>
2025-10-08 17:17:44 +0545

    cleaned up scaling issues for big and small screen

--- src/pages/README.txt
+++
@@ -1,2 +0,0 @@
-This is the directory that is the interface between backend and frontend.
-Hence it includes the pages and intermediatary api interfaces
---
+++ src/scripts/layout.ts
@@ -0,0 +1,6 @@
+import { customCursor }     from "@components/cursor/cursor";
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("overlord dom loaded! ")
+  customCursor();
+});
