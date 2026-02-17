5900722314e386a39f5675f426b0f58dd7e07356
aayush-lams <lamsal.aayush9115@gmail.com>
2026-02-17 11:58:04 +0545

    teminal ui

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,10 +1,6 @@
-import { customCursor }     from "@components/cursor/cursor";
-import { init_person_mode } from "@components/personMode/personmode";
-// import { addPopupEvents }   from "@components/popups/popup";
+// import { customCursor }     from "@components/cursor/cursor";
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("dom loaded! ")
-  customCursor();
-  init_person_mode();
-  // addPopupEvents();
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log("dom loaded! ")
+//   customCursor();
+// });
